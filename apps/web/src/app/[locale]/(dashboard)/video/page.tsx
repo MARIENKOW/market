37f676be0862b39a4f0fc1666b47420b4efcea6b
Denis Marienkow <den.marienkow@gmail.com>
2026-04-06 07:32:54 +0300

    fix

--- apps/web/src/app/[locale]/(dashboard)/video/page.tsx
+++ apps/web/src/app/[locale]/(dashboard)/video/page.tsx
@@ -1,22 +1,3 @@
-import BlogVideoService from "@/services/blog/video/blogVideo.service";
-import { $apiUserAxiosServer } from "@/utils/api/user/axios.user.server";
-import { getQueryClient } from "@/lib/tanstack/queryClient";
-import VideoComponentBlog from "@/components/TextEditor/components/video/VideoComponentBlog";
-import { videoKeys } from "@/lib/tanstack/keys";
-import { Hydrate } from "@/lib/tanstack/Hydrate";
-
-const { getAll } = new BlogVideoService($apiUserAxiosServer);
 export default async function Page() {
-    const queryClient = getQueryClient();
-
-    await queryClient.prefetchQuery({
-        queryKey: videoKeys.list({ page: 1 }),
-        queryFn: async () => (await getAll({ page: 1 })).data,
-    });
-
-    return (
-        <Hydrate>
-            <VideoComponentBlog />
-        </Hydrate>
-    );
+    return null;
 }
