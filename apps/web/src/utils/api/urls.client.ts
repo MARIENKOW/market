5860bbde6d3ef764968bc19e0e7648f6d83d8c25
Denis Marienkow <den.marienkow@gmail.com>
2026-04-07 07:57:12 +0300

    fix

--- apps/web/src/utils/api/urls.client.ts
+++ apps/web/src/utils/api/urls.client.ts
@@ -1,3 +1,5 @@
 import { clientEnv } from "@/config/env.client";
 
+console.log(clientEnv.NEXT_PUBLIC_API_ORIGIN_CLIENT);
+
 export const API_CLIENT_BASE_URL = `${clientEnv.NEXT_PUBLIC_API_ORIGIN_CLIENT}/${clientEnv.NEXT_PUBLIC_API_GLOBAL_PREFIX}`;
