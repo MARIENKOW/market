558b5a56c7d8386f727517d0891153c68b80f64f
Denis Marienkow <den.marienkow@gmail.com>
2026-05-08 19:34:30 +0300

    docker

--- apps/api/src/modules/app/app.controller.ts
+++ apps/api/src/modules/app/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { Public } from "@/modules/auth/decorators/public.decorator";
 
 @Controller()
-export class AppController {}
+export class AppController {
+    @Get("health")
+    @Public()
+    health() {
+        return { status: "ok" };
+    }
+}
